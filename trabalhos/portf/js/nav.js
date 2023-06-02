85d1b002536599c119bb61f2e65ff537802df3d0
Jack Kennedy <125822604+kennay-jks@users.noreply.github.com>
2023-06-02 00:32:27 -0300

    nav-mob finalizado

--- trabalhos/portf/js/nav.js
+++ trabalhos/portf/js/nav.js
@@ -5,10 +5,16 @@ nav.addEventListener('click', () => {
     if (id.classList.contains('active')) {
         id.classList.remove('active')
         id.classList.add('dea')
+        // icon
+        nav.classList.remove('active')
+        nav.classList.add('dea')
         //console.log('dea')
     } else {
         id.classList.remove('dea')
         id.classList.add('active')
+        // icon
+        nav.classList.remove('dea')
+        nav.classList.add('active')
         //console.log('act')
     }
 })
